1fe24bf35b8cb252ae767d5141af9d8fdfb7433b
RJ Garcia <ragboyjr@icloud.com>
2021-01-25 12:29:36 -0500

    updating links
    Signed-off-by: RJ Garcia <rj@stadiumgoods.com>

--- demo/src/Catalog/UI/Component/SizeScale/SizeScaleViewPage.php
+++ demo/src/Catalog/UI/Component/SizeScale/SizeScaleViewPage.php
@@ -5,7 +5,8 @@ namespace Demo\App\Catalog\UI\Component\SizeScale;
 use Demo\App\Catalog\Domain\SizeScale;
 use Krak\Admin\Templates\Layout\OneColumnLayout;
 use League\Plates\Component;
-use function Krak\Admin\Templates\Typography\{Button, ButtonLink, Card, DefinitionList, DefinitionListItem, PageTitle};
+use function Krak\Admin\Templates\Typography\{Button, ButtonLink, Card, DefinitionList, DefinitionListItem, PageTitle,
+    TextLink};
 use function League\Plates\Bridge\Symfony\path;
 use function League\Plates\{p, h};
 
@@ -22,15 +23,17 @@ final class SizeScaleViewPage extends Component
         echo (new OneColumnLayout(function() use ($title) {
         ?>
             <?=PageTitle($title)?>
-            <?=Card(DefinitionList([
-                DefinitionListItem('Id', $this->sizeScale->id()),
-                DefinitionListItem('Name', $this->sizeScale->name()),
-                DefinitionListItem('Status', $this->sizeScale->status()),
-            ]), ['class' => 'mt-4 max-w-lg'])?>
-            <div class="mt-4 space-x-2"><?=p([
-                ButtonLink('Back', path('catalog_size_scale_admin_list')),
-                ButtonLink('Edit', '#', 'success'),
-            ])?></div>
+            <?=Card([
+                DefinitionList([
+                    DefinitionListItem('Id', $this->sizeScale->id()),
+                    DefinitionListItem('Name', $this->sizeScale->name()),
+                    DefinitionListItem('Status', $this->sizeScale->status()),
+                ]),
+                h('div', [
+                    TextLink('Show All', path('catalog_size_scale_admin_list')),
+                    TextLink('Edit', '#'  )
+                ], ['class' => 'space-x-2 mt-4'])
+            ], ['class' => 'mt-4 max-w-lg p-4'])?>
         <?php
         }))->title($title);
     }
